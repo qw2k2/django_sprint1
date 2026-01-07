7f34a899a4ddcf638628b86a61076b1134a9c92b
qw2k2 <mi661507@gmail.com>
2026-01-07 19:00:37 +0300

    Update urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
-    path('', include('pages.urls', namespace='pages')),
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("pages/", include("pages.urls")),
 ]
