b5ab2fa2cac9df477e0942e82173647b395a3ffd
qw2k2 <mi661507@gmail.com>
2026-01-07 17:58:23 +0300

    update

---
+++ blogicum/blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, get_object_or_404
+
+posts = [
+    {
+        'id': 0,
+        'location': 'Остров отчаянья',
+        'date': '30 сентября 1659 года',
+        'category': 'travel',
+        'text': '''Наш корабль, застигнутый в открытом море
+                страшным штормом, потерпел крушение.
+                Весь экипаж, кроме меня, утонул; я же,
+                несчастный Робинзон Крузо, был выброшен
+                полумёртвым на берег этого проклятого острова,
+                который назвал островом Отчаяния.''',
+    },
+    {
+        'id': 1,
+        'location': 'Остров отчаянья',
+        'date': '1 октября 1659 года',
+        'category': 'not-my-day',
+        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
+                с мели приливом и пригнало гораздо ближе к берегу.
+                Это подало мне надежду, что, когда ветер стихнет,
+                мне удастся добраться до корабля и запастись едой и
+                другими необходимыми вещами.''',
+    },
+    {
+        'id': 2,
+        'location': 'Остров отчаянья',
+        'date': '25 октября 1659 года',
+        'category': 'not-my-day',
+        'text': '''Всю ночь и весь день шёл дождь и дул сильный
+                порывистый ветер. 25 октября. Корабль за ночь разбило
+                в щепки; на том месте, где он стоял, торчат какие-то
+                жалкие обломки.''',
+    },
+]
+
+def index(request):
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
+
+def post_detail(request, id):
+    post = get_object_or_404(posts, id=id)
+    return render(request, 'blog/detail.html', {'post': post})
+
+def category_posts(request, category_slug):
+    return render(request, 'blog/category.html', {'category_slug': category_slug})
