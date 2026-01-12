a72f6e5dad2ad106fb73e5c1cafd2421e218a2fb
qw2k2 <mi661507@gmail.com>
2026-01-12 15:32:05 +0300

    Update urls.py

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 
 from . import views
 
-app_name = "blog"
+app_name = 'blog'
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("posts/<int:id>/", views.post_detail, name="post_detail"),
+    path('', views.index, name='index'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path(
-        "category/<slug:category_slug>/",
+        'category/<slug:category_slug>/',
         views.category_posts,
-        name="category_posts",
+        name='category_posts',
     ),
 ]
